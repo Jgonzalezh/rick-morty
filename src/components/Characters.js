7ece34354945bcc7580fda5c3ba35d5e83dcdca3
Joaquín González Ham <jocgonham@gmail.com>
2025-01-13 16:04:12 -0300

    proyecto listo
    Rick

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,19 +1,47 @@
 
 
 export default function Characters(props) {
-    const {characters} = props;
+    const {characters, setCharacters} = props;
+    const restCharacters = () => {
+        setCharacters(null);
+    };
 
     return (
     <div className="characters">
       <h1>Personajes</h1>
-      <span className="back-home">Volver home</span>
+      <span className="back-home" onClick={restCharacters}>Volver home</span>
       <div  className="container-characters">
-        {characters.map((characters, index ) => (
+        {characters.map((character, index ) => (
             <div className="character-container" key={index}>
-                <p>{characters.name}</p>
+                <div><img src={character.images} alt={character.name}/>
+                    </div>
+                <div>
+                    <h3>{character.name}</h3>
+                    <h6>
+                        {character.status === "Alive" ? (
+                            <>
+                            <span className="alive"/>
+                            </>
+                        ): (
+                            <>
+                            <span className="dead"/>
+                            </>
+                        )                  
+                        }
+                        </h6>
+                    <p>
+                        <span className="text-grey">Episodios: </span>
+                        <span>{character.episode.lenght}</span>
+                    </p>
+                    <p>
+                    <span className="text-grey">Especie: </span>   
+                    <span>{character.species}</span> 
+                    </p>
+                    </div>
                 </div>
         ))}
       </div>
+      <span className="back-home" onClick={restCharacters}>Volver home</span>
     </div>
 
   );
